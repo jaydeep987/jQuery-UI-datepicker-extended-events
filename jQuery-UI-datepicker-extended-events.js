bf34f3303b2688420d688379354a376780d96717
jaydeep987 <jaydeep253a@gmail.com>
2015-04-23 02:56:25 -0700

    formatted

--- jQuery-UI-datepicker-extended-events.js
+++ jQuery-UI-datepicker-extended-events.js
@@ -23,13 +23,13 @@
 
 	/* Add afterShow event which will be fired after datepicker is updated. */
 	/* Override the _updateDatepicker method */
-	$.datepicker._updateDatepicker = function(inst)
-	{
+	$.datepicker._updateDatepicker = function(inst)	{
 		$.datepicker._updateDatepicker_original(inst);
 		var afterShow = this._get(inst, 'afterShow');
-		if (afterShow)
+		if (afterShow) {
 		    afterShow.apply((inst.input ? inst.input[0] : null),
 		    		[(inst.input ? inst.input.val() : ''), inst, inst.dpDiv.find('td:has(a)')]);  // trigger custom callback
+		}
 	};
 
 	/* 'before' Event for change month or year (called when value in select box change). */
@@ -37,23 +37,22 @@
 	$.datepicker._selectMonthYearOrig = $.datepicker._selectMonthYear;
 	
 	/* Override the _selectMonthYear method */
-	$.datepicker._selectMonthYear = function(id, select, period)
-	{
+	$.datepicker._selectMonthYear = function(id, select, period) {
 		var inst = this._getInst($(id)[0]);
 		var beforeChange = $.datepicker._get(inst, 'beforeChangeMonthYear');
-		if(beforeChange)
-		{
+		if(beforeChange) {
 			/* If event return true, then change to that month or year */
-			if(beforeChange.apply(this, [inst, select]))
+			if(beforeChange.apply(this, [inst, select])) {
 				this._selectMonthYearOrig(id, select, period);
+			}
 			/* If false, then don't change and keep old month or year selected */
-			else
-			{
+			else {
 				$(select).val(period == 'M' ? inst.selectedMonth : inst.selectedYear);
 			}
 		}
-		else
+		else {
 			this._selectMonthYearOrig(id, select, period);
+		}
 	};
 
 	/* 'before' Event for ajust date (called when < or > button clicked). */
@@ -61,16 +60,16 @@
 	$.datepicker._adjustDateOrig = $.datepicker._adjustDate;
 
 	/* Override the _adjustDate method */
-	$.datepicker._adjustDate = function(id, offset, period)
-	{
+	$.datepicker._adjustDate = function(id, offset, period)	{
 		var inst = this._getInst($(id)[0]);
 		var beforeAdjustDate = $.datepicker._get(inst, 'beforeAdjustDate');
-		if(beforeAdjustDate)
-		{
-			if(beforeAdjustDate.apply(this, [inst, offset, period]))
+		if(beforeAdjustDate) {
+			if(beforeAdjustDate.apply(this, [inst, offset, period])) {
 				this._adjustDateOrig(id, offset, period);
+			}
 		}
-		else
+		else {
 			this._adjustDateOrig(id, offset, period);
+		}
 	};
 }(jQuery));
